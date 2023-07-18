be6b266ef1022034c82904b8d649398bc85425ab
Tejasvee <tejasvee.rathour29aug@gmail.com>
2023-07-18 11:22:24 +0530

    Fifth Commit

---
+++ server/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+export const Connection = async (username, password) => {
+    const URL = `mongodb://${username}:${password}@ac-selmzgk-shard-00-00.jpjg57u.mongodb.net:27017,ac-selmzgk-shard-00-01.jpjg57u.mongodb.net:27017,ac-selmzgk-shard-00-02.jpjg57u.mongodb.net:27017/?ssl=true&replicaSet=atlas-tf28wj-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log('Database connected Successfully');
+    }
+    catch (error) {
+        console.log('Error while connection with the database', error.message);
+    }
+}
+
+export default Connection;
